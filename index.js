30f75dcf526b0eba280fee3e1fb442d3bee9d88a
DavidJulianGit <DavidJulianGit@users.noreply.github.com>
2024-01-28 15:36:44 +0100

    Removing "next()", Dynamic port configuration, Logging to log.txt instead of the terminal

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
+// Importing movie data
 const movieData = require('./data/movieData.json');
-
+// Requirements
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs');
 const path = require('path');
 
+// Dynamic port configuration
+const port = process.env.PORT || 8080;
+
+// Create the Express application
 const app = express();
 
-// Logging Timestamps, req method, url, res code
+// create a write stream (in append mode) to a file
+// called "log.txt", which is created in the same directory as this very file.
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
+  flags: 'a',
+});
+
+// Setup the logger to write to "log.txt"
+app.use(morgan('myformat', { stream: accessLogStream }));
 app.use(morgan('common'));
 
 // Setting default file directory for serving static files
 app.use(express.static('public'));
 
 // Response for /movies - json data about top 10 movies
-app.get('/movies', (req, res, next) => {
+app.get('/movies', (req, res) => {
   res.json(movieData);
-  next();
 });
 
 // Response for index
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.send(
-    'Welcome to MYFLIX - your favorite source of information about the movies you love.',
+    'Welcome to MYFLIX - your favorite source of inforkmation about the movies you love.',
   );
-  next();
 });
 
 // Error handling
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-// listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+
+// Listen for requests
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}.`);
 });

b7cee87c22a3045696b0b5b58b5a55511a24abc7
DavidJulianGit <DavidJulianGit@users.noreply.github.com>
2024-06-08 13:25:52 +0200

    Back to working code.

--- index.js
+++ index.js
@@ -42,13 +42,12 @@ app.use(cors({
    origin: (origin, callback) => {
       console.log('Incoming origin:', origin);
       if (!origin) return callback(null, true);
-      if (origin.includes('localhost') || allowedOrigins.includes(origin)) {
-         return callback(null, true);
-      }
-      else {
+
+      if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
          let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
          return callback(new Error(message), false);
       }
+      return callback(null, true);
    }
 }));
 
